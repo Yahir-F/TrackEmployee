04c36c075d530ef8d830cb7ee845cba7adb2d646
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-28 18:14:59 -0700

    created seed data

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,20 @@
-USE employeeData_db;
+INSERT INTO worker(worker_name,worker_last,role_id)
+VALUES 
+       ("bob", "Cena", null, 1),
+        ("jeffrey", "hashbrown", 2, 4),
+       ("Under","Taker", 1, 4),
+       ("Big", "tech", 2, 8),
+       ("Yahir", "newDeveloper", 5, 3),
+      
+      INSERT INTO  role (title, pay, department)
+      VALUES ('Software Engineer', 170000, 2),
+             ('Web Developer', 130000, 1),
+             ('Management', 250000),
+             ('Code Monkey', 1000000000, 4),
+             ('Intern', 1,  3);
+
+INSERT INTO (name)
+VALUES ('Developer'),
+        ('Engineer'),
+        ('HR'),
+        ('Internship'),
